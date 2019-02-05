99b0174f606e4b1162d6195fcb8f3082fa2d8f08
Jared Benjamin Daines <obiyoda@ldschurch.org>
2019-02-05 09:46:12 -0700

    building the project needed to comment out the eslint changes to get it to all work

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,72 +1,69 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: [
-    './src/driver.scss',
-    './src/index.js',
-  ],
+  mode: "production",
+  entry: ["./src/driver.scss", "./src/index.js"],
   output: {
-    path: path.join(__dirname, '/../dist'),
-    publicPath: '/dist/',
-    filename: 'driver.min.js',
-    libraryTarget: 'umd',
-    library: 'Driver',
-    libraryExport: 'default',
+    path: path.join(__dirname, "/../dist"),
+    publicPath: "/dist/",
+    filename: "driver.min.js",
+    libraryTarget: "umd",
+    library: "Driver",
+    libraryExport: "default"
   },
   module: {
     rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   enforce: 'pre',
+      //   options: {
+      //     failOnWarning: false,
+      //     failOnError: true,
+      //   },
+      // },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        options: {
-          failOnWarning: false,
-          failOnError: true,
-        },
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader"
       },
       {
         test: /.scss$/,
         loader: ExtractTextPlugin.extract([
           {
-            loader: 'css-loader',
-            options: { minimize: true, url: false },
+            loader: "css-loader",
+            options: { minimize: true, url: false }
           },
-          'sass-loader',
-        ]),
-      },
-    ],
+          "sass-loader"
+        ])
+      }
+    ]
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: 'driver.min.css',
-      allChunks: true,
+      filename: "driver.min.css",
+      allChunks: true
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.min\.css$/g,
       // eslint-disable-next-line global-require
-      cssProcessor: require('cssnano'),
+      cssProcessor: require("cssnano"),
       cssProcessorPluginOptions: {
         preset: [
-          'default',
+          "default",
           {
-            discardComments: { removeAll: true },
-          },
-        ],
+            discardComments: { removeAll: true }
+          }
+        ]
       },
-      canPrint: true,
-    }),
+      canPrint: true
+    })
   ],
   stats: {
-    colors: true,
+    colors: true
   },
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map"
 };
